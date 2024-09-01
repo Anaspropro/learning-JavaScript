887555c81acd87dbb382d97a807069f166ff00cc
Webwiz <aliuanas004@gmail.com>
2024-09-01 07:15:43 +0100

    playing around...

---
+++ script.js
@@ -0,0 +1,21 @@
+// function sum (a, b){
+//   return a + b;
+// }
+
+// let result = sum(1, 2);
+// // console.log(result);  
+
+// function checkAge(age){
+//   if (age >= 18){
+//     return true;
+//   } else {
+//     return confirm("Do you have your parent's permission?" )
+//   }
+// }
+
+// let age = prompt('How old are you?')
+// if (checkAge(age)){
+//   alert(' Access granted ');
+// } else {
+//   alert(' Access denied ');
+// }
